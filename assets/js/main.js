851e358039629afc5083d8ea9e8e79f9fe748b2d
vasilie <vasilie.91@gmail.com>
2016-03-23 00:26:21 +0100

    finished helicopter modeling, physics

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,54 +19,113 @@ window.addEventListener("keyup", function(e){
   delete keys[e.keyCode];
 }, false);
 
-//Helicopter
+/*==============================================*\
+  Helicopter
+\*==============================================*/
+
 var helicopter_material = new THREE.MeshPhongMaterial( {
    color: 0xFFD83B
- });
- var helicopter_material_dark = new THREE.MeshPhongMaterial( {
+ }),
+    helicopter_material_dark = new THREE.MeshPhongMaterial( {
     color: 0x7B711B
-  });
- var propeler_material = new THREE.MeshPhongMaterial( {
+  }),
+    propeler_material = new THREE.MeshPhongMaterial( {
     color: 0x333333
   });
-var h_cockpit_geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var h_cockpit_medium_geometry = new THREE.BoxGeometry( 0.9, 0.9, 1.4 );
-var h_cockpit_small_geometry = new THREE.BoxGeometry( 0.7, 0.7, 1 );
-var h_tail_head_geometry = new THREE.BoxGeometry( 0.3, 0.5, 0.2 );
-var h_tail_arm_geometry = new THREE.BoxGeometry( 0.3, 0.3, 1.4 );
-var h_tail_hand_geometry = new THREE.BoxGeometry( 0.3, 0.5, 0.4 );
-var h_propeler_base_geometry = new THREE.BoxGeometry( 0.2, 0.1, 0.2 );
-var h_propeler_pin_geometry = new THREE.BoxGeometry( 0.05, 0.1, 0.05 );
-var h_propeler_geometry = new THREE.BoxGeometry( 4, 0.03,0.13 );
-var h_propeler2_geometry = new THREE.BoxGeometry( 0.13, 0.03, 4 );
+var h_cockpit_geometry = new THREE.BoxGeometry( 1, 1, 1 ),
+    h_cockpit_medium_geometry = new THREE.BoxGeometry( 0.9, 0.9, 1.4 ),
+    h_cockpit_small_geometry = new THREE.BoxGeometry( 0.7, 0.7, 1 ),
 
-var h_tail_hand = new THREE.Mesh(h_tail_hand_geometry, propeler_material);
-var h_propeler = new THREE.Mesh(h_propeler_geometry, propeler_material);
-var h_propeler2 = new THREE.Mesh(h_propeler2_geometry, propeler_material);
-var h_propeler_pin = new THREE.Mesh(h_propeler_pin_geometry, helicopter_material);
-var h_propeler_base = new THREE.Mesh(h_propeler_base_geometry, helicopter_material);
-var h_tail_arm = new THREE.Mesh(h_tail_arm_geometry, helicopter_material);
-var h_tail_head = new THREE.Mesh(h_tail_head_geometry, helicopter_material);
-var h_cockpit_small = new THREE.Mesh(h_cockpit_small_geometry, helicopter_material);
-var h_cockpit_medium = new THREE.Mesh(h_cockpit_medium_geometry, helicopter_material);
-var h_cockpit = new THREE.Mesh(h_cockpit_geometry, helicopter_material_dark);
+    h_tail_head_geometry = new THREE.BoxGeometry( 0.3, 0.5, 0.2 ),
+    h_tail_arm_geometry = new THREE.BoxGeometry( 0.3, 0.3, 1.4 ),
+    h_tail_hand_geometry = new THREE.BoxGeometry( 0.3, 0.5, 0.5 ),
+
+    h_propeler_base_geometry = new THREE.BoxGeometry( 0.2, 0.1, 0.2 ),
+    h_propeler_pin_geometry = new THREE.BoxGeometry( 0.05, 0.1, 0.05 ),
+    h_propeler_geometry = new THREE.BoxGeometry( 4, 0.03,0.13 ),
+    h_propeler2_geometry = new THREE.BoxGeometry( 0.13, 0.03, 4 ),
+
+    h_tail_propeler_geometry = new THREE.BoxGeometry( 0.02, 0.45, 0.07 ),
+    h_tail_propeler2_geometry = new THREE.BoxGeometry( 0.02, 0.07, 0.45 ),
+    h_tail_propeler_pin_geometry = new THREE.BoxGeometry( 0.1, 0.05, 0.05 ),
+
+    h_rails_vertical_geometry = new THREE.BoxGeometry( 0.1, 0.4, 0.1 ),
+    h_rails_horizontal_geometry = new THREE.BoxGeometry( 0.16, 0.05, 1.4 ),
+
+
+    h_rails = new THREE.Mesh(),
+    h_rails2 = new THREE.Mesh(),
+
+    h_rails_vertical = new THREE.Mesh(h_rails_vertical_geometry, propeler_material),
+    h_rails_vertical2 = new THREE.Mesh(h_rails_vertical_geometry, propeler_material),
+    h_rails_horizontal = new THREE.Mesh(h_rails_horizontal_geometry, propeler_material),
+
+    h_rails2_vertical = h_rails_vertical.clone(),
+    h_rails2_vertical2 = h_rails_vertical.clone(),
+    h_rails2_horizontal = h_rails_horizontal.clone();
+
+    h_rails.add(h_rails_vertical, h_rails_vertical2, h_rails_horizontal);
+    h_rails2.add(h_rails2_vertical, h_rails2_vertical2, h_rails2_horizontal);
+
+var h_propeler = new THREE.Mesh(h_propeler_geometry, propeler_material),
+    h_propeler2 = new THREE.Mesh(h_propeler2_geometry, propeler_material),
+    h_propeler_pin = new THREE.Mesh(h_propeler_pin_geometry, helicopter_material),
+    h_propeler_base = new THREE.Mesh(h_propeler_base_geometry, helicopter_material),
+
+    h_tail_propeler = new THREE.Mesh(h_tail_propeler_geometry, helicopter_material_dark),
+    h_tail_propeler2 = new THREE.Mesh(h_tail_propeler2_geometry, helicopter_material_dark),
+    h_tail_propeler_pin = new THREE.Mesh(h_tail_propeler_pin_geometry, propeler_material),
+
+    h_tail_hand = new THREE.Mesh(h_tail_hand_geometry, propeler_material),
+    h_tail_arm = new THREE.Mesh(h_tail_arm_geometry, helicopter_material),
+    h_tail_head = new THREE.Mesh(h_tail_head_geometry, helicopter_material),
+
+    h_cockpit_small = new THREE.Mesh(h_cockpit_small_geometry, helicopter_material),
+    h_cockpit_medium = new THREE.Mesh(h_cockpit_medium_geometry, helicopter_material),
+    h_cockpit = new THREE.Mesh(h_cockpit_geometry, helicopter_material_dark);
+
 var helicopter = new THREE.Mesh();
 
+// h_rails_horizontal.position.set(0, 0, -0.1);
+// h_rails_horizontal.position.set(0, 0, -0.1);
+
+
+h_rails_vertical.position.set(0, 0.20, 0.4);
+h_rails_vertical2.position.set(0, 0.20, -0.4);
+h_rails2_vertical.position.set(0, 0.20, 0.4);
+h_rails2_vertical2.position.set(0, 0.20, -0.4);
+h_rails.position.set(0.5,-0.80,0);
+h_rails2.position.set(-0.5,-0.80,0);
+
+h_tail_propeler_pin.position.x = 0.15;
+h_tail_propeler.position.x = 0.20;
+h_tail_propeler.rotation.x = 0.24
+
+h_rails.rotation.z = 0.4;
+h_rails2.rotation.z = -0.4;
+
+h_rails_horizontal.rotation.z = -0.4;
+h_rails2_horizontal.rotation.z = 0.4;
 
 h_propeler.position.y = 0.7;
 h_propeler_pin.position.y = 0.65;
 h_propeler_base.position.y = 0.55;
-h_tail_hand.position.z = -2.5;
+
+h_tail_hand.position.z = -2.55;
 h_tail_hand.position.y = 0.26;
 h_tail_arm.position.z = -1.6;
 h_tail_arm.position.y = 0.16;
 h_tail_head.position.y = 0.06;
 h_tail_head.position.z = -0.8;
+
 h_cockpit_small.position.z = 0.4;
 
+h_tail_propeler.add( h_tail_propeler2);
+h_tail_hand.add(h_tail_propeler, h_tail_propeler_pin);
 h_propeler.add(h_propeler2);
-helicopter.add(h_cockpit, h_cockpit_medium, h_cockpit_small, h_tail_head, h_tail_arm,h_tail_hand,h_propeler_base, h_propeler_pin,h_propeler);
+helicopter.add(h_cockpit, h_cockpit_medium, h_cockpit_small, h_tail_head, h_tail_arm,h_tail_hand,h_propeler_base, h_propeler_pin,h_propeler, h_rails, h_rails2);
 scene.add(helicopter);
+// helicopter.add(camera);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -124,9 +183,9 @@ wall5.position.z = -3400;
 wall5.position.x =0;
 wall5.position.y =2.6;
 // //
-controls = new THREE.OrbitControls( camera, renderer.domElement );
-				controls.target.set( 0, 5, 0 );
-				controls.update();
+// controls = new THREE.OrbitControls( camera, renderer.domElement );
+// 				controls.target.set( 0, 5, 0 );
+// 				controls.update();
 var ambient = new THREE.AmbientLight( 0xF8FFED);
 ambient.intensity = 0.6;
 scene.add(ambient);
@@ -286,11 +345,15 @@ box.velocity = -5;
 camera.position.z = 3;
 camera.position.y = 3;
 camera.position.x = 3;
-
+helicopter.velocityZ = 0;
+helicopter.velocityY = 0;
 // camera.position.y = 2;
+h_propeler.angularVelocity = 0;
+// helicopter.add(camera);
+
 box.add(propeler);
 propeler.add(propeler2);
-camera.lookAt(helicopter.position);
+// camera.lookAt(helicopter.position);
 // camera.lookAt(scene.position);
 function render() {
 	requestAnimationFrame( render );
@@ -299,10 +362,15 @@ function render() {
   }
   // console.log(propeler.position.z);
   box.rotation.y = 0;
-  if(keys[65]){box.position.x-=box.velocityZ/6; box.rotation.y += 0.004; h_propeler.rotation.y += 0.4;} // Left
-  if(keys[68]){box.position.x+=box.velocityZ/6;box.rotation.y  -= 0.004;} // Right
-  if(keys[87] ){box.velocityZ+=0.04; }// Up
-  if(keys[83] ){box.velocityZ-=0.04; } // down
+  if(keys[65]){helicopter.position.x-=helicopter.velocityZ/6; helicopter.rotation.y += 0.004; h_propeler.rotation.y += 0.4;} // Left
+  if(keys[68]){helicopter.position.x+=helicopter.velocityZ/6;helicopter.rotation.y  -= 0.004;} // Right
+  if(keys[87] ){helicopter.rotationXx+=0.01; }// Up
+  if(keys[83] ){helicopter.rotation.x-=0.01; }// Down
+
+  if(keys[81] ){helicopter.rotation.y+=0.04; } // Q
+  if(keys[69] ){helicopter.rotation.y-=0.04; } // E
+
+
   if(keys[16] ){shoot();} // down
   // camera.position.x = box.position.x;
   // camera.position.y = box.position.y+0.5;
@@ -310,28 +378,28 @@ function render() {
   // camera.lookAt(box.position);
 
 
-  if(keys[38] || keys[32] ){propeler.rotation.y+=1.25, box.velocity +=0.012; }
+  if(keys[38] || keys[32] ){h_propeler.angularVelocity+=0.01; console.log(h_propeler.angularVelocity) }
+
   // if(keys[40] || keys[17] ){box.position.y-=0.05; }
   if(keys[39]){box.rotation.y+=0.05;}
   if(keys[37]){box.rotation.y-=0.05;}
   if (box.rotation.y >= 0.04){box.rotation.y = 0.04}
   if (box.rotation.y <= - 0.04){box.rotation.y = -0.04}
   // scene.rotation.y += 0.01;
-  box.velocity -=0.01;
-  if (box.velocityZ >0){
-    box.velocityZ -= 0.01;
-  } else {
-    box.velocityZ = 0;
+  // helicopter.velocityY -=0.01;
+  if (helicopter.position.y >0.76){
+    helicopter.velocityY -= 0.008;
+  }  else if (helicopter.position.y <0.76){
+    helicopter.velocityY = 0;
+    helicopter.position.y = 0.76;
   }
   if (box.velocityZ >= 2.7){
     box.velocityZ  = 2.7;
   }
 
-  box.position.y += box.velocity;
-  if (box.position.y <= 0){
-    box.velocity = 0;
-    box.position.y =0;
-  }
+  if( h_propeler.angularVelocity >0.7){helicopter.velocityY +=0.015}
+    helicopter.position.y += helicopter.velocityY;
+    helicopter.velocity = helicopter.rotation.x *0.3;
   if (box.position.z<-7000){
     box.position.z = 0;
   }
@@ -355,12 +423,43 @@ function render() {
       bullets.splice(i,1);
     }
   }
+  if (h_propeler.angularVelocity > 0){
+    h_propeler.angularVelocity -=0.001;
+  } else {
+    h_propeler.angularVelocity = 0;
+  }
+  if (h_propeler.angularVelocity > 0.7){
+    h_propeler.angularVelocity = 0.7;
+  }
+  h_propeler.rotation.y+=h_propeler.angularVelocity;
   // camera.lookAt(box.position);
   // propeler.position.x = box.position.x;
   // propeler.position.y = box.position.y - 1;
   // propeler.position.z = box.position.z;
   // text2.innerHTML = "VELOCITYZ:"+box.velocityZ;
+  // camera.position.set(10, 10, 10);
+  // camera.lookAt(helicopter.position);
+  // helicopter.position.z = 20;
+  // camera.position.z = 30;
+  if (helicopter.rotation.x> 0){
+    helicopter.rotation.x -=0.004;
+  } else if(helicopter.rotation.x < 0){
+    helicopter.rotation.x +=0.004;
+  }
+
+  var vBx = helicopter.velocity*Math.cos(helicopter.rotation.y); //Speed of bullet on x-axis
+  var vBz = helicopter.velocity*Math.sin(helicopter.rotation.y);
+
+  helicopter.position.z+=vBx;
+  helicopter.position.x+=vBz;
+  box.position.z = -20;
+
+  camera.position.x = helicopter.position.x;
+  camera.position.y = helicopter.position.y+3;
+  camera.position.z = helicopter.position.z-10;
+  camera.lookAt(helicopter.position);
 	renderer.render( scene, camera );
+  // console.log(helicopter.rotation);
 }
 
 
